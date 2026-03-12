869461b5bf512c242066980638afffb0a458bb65
dfialkov <54775560+dfialkov@users.noreply.github.com>
2026-03-11 18:11:17 -0700

    Bump gongfu-timer to v1.1.0

--- Casks/gongfu-timer.rb
+++ Casks/gongfu-timer.rb
@@ -1,6 +1,6 @@
 cask "gongfu-timer" do
-  version "1.0.0"
-  sha256 "a4d8e089da7fba0e4f0d96a5767ae8d97b9d8681d9f200493b4f92acb33f832c"
+  version "1.1.0"
+  sha256 "243efed972ca3f27e632e489dc254d42df265525dca0ea639098d239eb5cb997"
 
   url "https://github.com/dfialkov/taskbar-gongfu-timer/releases/download/v#{version}/GongfuTimer-#{version}.zip"
   name "Gongfu Timer"
