2c91a2ff26c2a9cde64e6cbe40198f392bc78c15
dfialkov <54775560+dfialkov@users.noreply.github.com>
2026-03-28 18:09:34 -0700

    Bump gongfu-timer to 1.2.0

--- Casks/gongfu-timer.rb
+++ Casks/gongfu-timer.rb
@@ -1,13 +1,13 @@
 cask "gongfu-timer" do
-  version "1.1.0"
-  sha256 "243efed972ca3f27e632e489dc254d42df265525dca0ea639098d239eb5cb997"
+  version "1.2.0"
+  sha256 "acb9a6d26911bfee70b6a1636fe282ffca954ab82948095c9a7463bfa62af14e"
 
   url "https://github.com/dfialkov/taskbar-gongfu-timer/releases/download/v#{version}/GongfuTimer-#{version}.zip"
   name "Gongfu Timer"
   desc "Menu bar timer for gongfu tea brewing"
   homepage "https://github.com/dfialkov/taskbar-gongfu-timer"
 
-  depends_on macos: ">= :sonoma"
+  depends_on macos: ">= :tahoe"
 
   app "GongfuTimer.app"
 end
